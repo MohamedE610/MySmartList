2f7a15bd0097a420041929ffa45f36b72ef5f956
Mohamed Mostafa <mohamedm9595@gmail.com>
2018-03-01 15:57:54 +0200

    Add retrofit

--- app/src/main/java/com/example/mysmartlist/Utils/Networking/RetrofitUtils/ApiInterface.java
+++ app/src/main/java/com/example/mysmartlist/Utils/Networking/RetrofitUtils/ApiInterface.java
@@ -4,6 +4,7 @@ package com.example.mysmartlist.Utils.Networking.RetrofitUtils;
 
 
 import com.example.mysmartlist.Models.Product;
+import com.example.mysmartlist.Utils.Constants;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -14,9 +15,6 @@ import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
-/**
- * Created by abdallah on 12/18/2017.
- */
 
 public interface ApiInterface {
 
@@ -36,41 +34,105 @@ public interface ApiInterface {
 
     /***************         products           *****************/
 
-
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @DELETE("clients/{client_id}/pin/{product_id}")
     Call<Product> deletePinProduct(@Path("client_id") int client_id,@Path("product_id") int product_id);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @DELETE("clients/{client_id}/fav/{product_id}")
     Call<Product> deleteFavouriteProduct(@Path("client_id") int client_id,@Path("product_id") int product_id);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     //pass json{}
     @POST("products")
     Call<Product> addProductToDatabase(@Body Product  product);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @POST("products/search")
     Call<Product> getProductSearchResult(@Body Product  product);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("products")
     Call<Product> getProducts();
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("new/products")
     Call<Product> getNewProducts();
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("popular/products")
     Call<Product> getPopularProducts();
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("top/products")
     Call<Product> getTopProducts();
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("products/{product_id}")
     Call<Product> getProductByID(@Path("product_id") int product_id);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("categories/{category_id}")
     Call<Product> getProductByCategory(@Path("category_id") int category_id);
 
 
     /***************         Categories           *****************/
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("categories")
     Call<Product> getCategories();
 
@@ -80,24 +142,66 @@ public interface ApiInterface {
 
     /***************         clients           *****************/
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("/clients/{client_id}")
     Call<Product> getClientByID(@Path("client_id") int client_id);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("clients/{client_id}/lists")
     Call<Product> getCurrentClientLists(@Path("client_id") int client_id);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("clients/{client_id}/lists/old")
     Call<Product> getOldClientLists(@Path("client_id") int client_id);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("clients/{client_id}/lists/all")
     Call<Product> getAllClientLists(@Path("client_id") int client_id);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("clients/{client_id}/fav/{product_id}")
     Call<Product> AddFavouriteProduct(@Path("client_id") int client_id ,@Path("product_id") int product);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("clients/{client_id}/pin/{product_id}")
     Call<Product> AddPinProduct(@Path("client_id") int client_id ,@Path("product_id") int product);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     //pass json{}
     @POST("clients")
     Call<Product> createClientAccount(@Body Product  client);
@@ -105,31 +209,84 @@ public interface ApiInterface {
 
     /***************         List           *****************/
 
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @GET("lists/{list_id}")
     Call<Product> getListById(@Path("list_id") int client_id);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     //pass json{}
     @POST("clients/{client_id}/lists")
     Call<Product> addListToDatabase(@Path("client_id") String client_id ,@Body Product  list);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @POST("lists/{list_id}/products")
     Call<Product> addProductToList(@Path("list_id") String client_id ,@Body Product  list);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @POST("lists/{list_id}/products/{product_id}/increment")
     Call<Product> incrementProductCountInList(@Path("list_id") String client_id ,@Path("product_id") String product_id ,@Body Product  list);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @POST("lists/{list_id}/products/{product_id}/decrement")
     Call<Product> decrementProductCountInList(@Path("list_id") String client_id ,@Path("product_id") String product_id ,@Body Product  list);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @DELETE("lists/{list_id}/products")
     Call<Product> deleteMultipulProduct(@Path("list_id") int client_id,@Body Product  list);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @DELETE("lists/{list_id}/products/{product_id}")
     Call<Product> deleteSingleProduct(@Path("list_id") int client_id,@Path("product_id") String product_id );
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @DELETE("lists/{list_id}/trash")
     Call<Product> MoveListFromCurrentListToOldList(@Path("list_id") int client_id);
 
+
+    @Headers({
+            "Accept: application/json",
+            "Content-Type: application/json",
+            "Authorization: "+ Constants.accessToken
+    })
     @DELETE("lists/{list_id}")
     Call<Product> deleteListPermanently(@Path("list_id") int client_id);
 
