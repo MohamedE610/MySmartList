85f905ef95e06dda0a26e5b506f23b8bf40f7776
Mohamed Mostafa <mohamedm9595@gmail.com>
2018-03-26 18:06:40 +0200

    add web crawler

--- app/src/main/java/com/example/mysmartlist/Activities/ReportsActivity.java
+++ app/src/main/java/com/example/mysmartlist/Activities/ReportsActivity.java
@@ -1,15 +1,27 @@
 package com.example.mysmartlist.Activities;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mysmartlist.Fragments.OLdListsFragment;
+import com.example.mysmartlist.Fragments.ReportsFragment;
 import com.example.mysmartlist.R;
 
 public class ReportsActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reports);
+        addReportsFragment();
+    }
+
+    private void addReportsFragment() {
+        fragmentManager=getSupportFragmentManager();
+        ReportsFragment reportsFragment=new ReportsFragment();
+        fragmentManager.beginTransaction().add(R.id.old_lists_container,reportsFragment).commit();
     }
 }
